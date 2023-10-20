6ca3ce24c260bb814be7551ce48f8abf8b6d983e
hasan13222 <jamil8305@gmail.com>
2023-10-20 09:24:06 +0600

    single product get api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,7 +36,12 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
-
+    app.get('/products/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await brandProducts.findOne(query);
+      res.send(result);
+    })
     app.get("/productAds", async(req, res) => {
         const cursor = productAds.find();
         const result = await cursor.toArray();
