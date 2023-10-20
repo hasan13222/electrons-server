6842a01606786e543b945eb08410636a212df226
hasan13222 <jamil8305@gmail.com>
2023-10-20 11:33:11 +0600

    cart delete api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterelectorn.nbq5yot.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,43 +31,51 @@ async function run() {
     const productAds = database.collection("productAds");
     const cartProducts = database.collection("cartProducts");
 
-    app.get("/products", async(req, res) => {
-        const cursor = brandProducts.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    app.get('/product/:id', async(req, res) => {
+    app.get("/products", async (req, res) => {
+      const cursor = brandProducts.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await brandProducts.findOne(query);
       res.send(result);
-    })
-    app.get("/productAds", async(req, res) => {
-        const cursor = productAds.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-    app.post('/product', async (req, res) => {
-        const newProduct = req.body;
-        const result = await brandProducts.insertOne(newProduct);
-        res.send(result);
-    })
-    
-    app.get("/carts", async(req, res) => {
+    });
+    app.get("/productAds", async (req, res) => {
+      const cursor = productAds.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await brandProducts.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
       const cursor = cartProducts.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.post("/addToCart", async (req, res) => {
+      const newProduct = req.body;
+      const result = await cartProducts.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.delete('/cart/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await cartProducts.deleteOne(query);
+      res.send(result);
   })
-
-    app.post('/addToCart', async (req, res) => {
-        const newProduct = req.body;
-        const result = await cartProducts.insertOne(newProduct);
-        res.send(result);
-    })
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -76,11 +83,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("electron is running")
-})
+app.get("/", (req, res) => {
+  res.send("electron is running");
+});
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-})
+  console.log(`listening on ${port}`);
+});
